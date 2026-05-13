92ed6fed1454f7b4ee42811b7b7b649522ed4fd3
jacktoyour <jacktoyour@gmail.com>
2026-05-13 11:39:49 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("EC2 Streamlit 배포 성공")
+st.write("AWS EC2에서 실행되는 Streamlit 앱입니다.")
+name=st.text_input("이름을 입력하세요")
+if st.button("확인"):
+    st.success(f"{name}님, EC2 배포 실습 성공")
